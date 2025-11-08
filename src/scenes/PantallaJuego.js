cbac653595332e23f915edad11740669dfca2c37
Claudia <148151287+CF-2003@users.noreply.github.com>
2025-11-08 01:11:52 +0100

    cronometro

--- src/scenes/PantallaJuego.js
+++ src/scenes/PantallaJuego.js
@@ -27,7 +27,7 @@ export class PantallaJuego extends Phaser.Scene {   //Crear clase que hereda de 
         this.load.spritesheet('GanchoIdle', 'Assets/Sprites/gancho.png', { frameWidth: 108, frameHeight: 50 });
 
     }
-    create() {   
+    create() {
 
         //Fondo
         const background = this.add.image(0, 0, 'BackgroundGraveyard').setOrigin(0); //Añadir imagen de fondo
@@ -71,27 +71,27 @@ export class PantallaJuego extends Phaser.Scene {   //Crear clase que hereda de 
 
         //Paredes
         //Lo mismo que piso pero para paredes
-        this. LeftWall = this.add.rectangle(0, this.scale.height / 2, 180, this.scale.height/* ,0x00ff00*/);
-        this.physics.add.existing(this.LeftWall, true); 
-        this. RightWall = this.add.rectangle(this.scale.width, this.scale.height / 2, 180, this.scale.height/*,0x00ff00*/);
+        this.LeftWall = this.add.rectangle(0, this.scale.height / 2, 180, this.scale.height/* ,0x00ff00*/);
+        this.physics.add.existing(this.LeftWall, true);
+        this.RightWall = this.add.rectangle(this.scale.width, this.scale.height / 2, 180, this.scale.height/*,0x00ff00*/);
         this.physics.add.existing(this.RightWall, true);
 
         //Tubo del gancho
-        this.TuboGancho = this.add.rectangle(this.scale.width/2, 80, this.scale.width-180, 20, 0x00ff00);
+        this.TuboGancho = this.add.rectangle(this.scale.width / 2, 80, this.scale.width - 180, 20, 0x00ff00);
         this.physics.add.existing(this.TuboGancho, true);
 
         //Crear Ania
         this.Ania = this.physics.add.sprite(42, 25, 'AniaIdle'); //Crear sprite de Ania
         this.Ania.setScale(1.5).setFrame(1); //Escalar y poner frame inicial
         this.Ania.y = 150; //Posición inicial Y
-        this.Ania.x=this.scale.width/2; //Posición inicial X
+        this.Ania.x = this.scale.width / 2; //Posición inicial X
         this.Ania.name = "Ania";
 
         //Crear Gancho
         this.Gancho = this.physics.add.sprite(108, 50, 'GanchoIdle').setImmovable(true);
         this.Gancho.setScale(1.5).setFrame(1);
         this.Gancho.y = 80;
-        this.Gancho.x = this.scale.width/2;
+        this.Gancho.x = this.scale.width / 2;
         this.Gancho.setOrigin(0.5, 0.1); // 0.1 lo pone el pivote cerca de la parte superior
         this.Gancho.body.setAllowGravity(false); //Desactivar gravedad
         this.Gancho.anims.play('Anim_GanchoIdle', true);
@@ -99,43 +99,43 @@ export class PantallaJuego extends Phaser.Scene {   //Crear clase que hereda de 
         //Crear plataformas
         this.platform1 = this.physics.add.image(146, 14, 'PlataformasBajas').setScale(1.5).setImmovable(true);
         this.platform1.body.setAllowGravity(false);
-        this.platform1.x=250;
-        this.platform1.y=420;
+        this.platform1.x = 250;
+        this.platform1.y = 420;
 
         this.platform2 = this.physics.add.image(146, 14, 'PlataformasBajas').setScale(1.5).setImmovable(true);
         this.platform2.body.setAllowGravity(false);
-        this.platform2.x=this.scale.width-250;
-        this.platform2.y=420;
+        this.platform2.x = this.scale.width - 250;
+        this.platform2.y = 420;
 
         this.platform3 = this.physics.add.image(183, 14, 'PlataformasBajas').setScale(1.5).setImmovable(true);
         this.platform3.body.setAllowGravity(false);
-        this.platform3.x=this.scale.width/2-35;
-        this.platform3.y=330;
+        this.platform3.x = this.scale.width / 2 - 35;
+        this.platform3.y = 330;
 
         this.platform4 = this.physics.add.image(183, 14, 'PlataformasBajas').setScale(1.5).setImmovable(true);
         this.platform4.body.setAllowGravity(false);
-        this.platform4.x=this.scale.width/2+35;
-        this.platform4.y=330;
+        this.platform4.x = this.scale.width / 2 + 35;
+        this.platform4.y = 330;
 
         this.platform5 = this.physics.add.image(183, 14, 'PlataformasBajas').setScale(1.5).setImmovable(true);
         this.platform5.body.setAllowGravity(false);
-        this.platform5.x=315;
-        this.platform5.y=265;
+        this.platform5.x = 315;
+        this.platform5.y = 265;
 
         this.platform6 = this.physics.add.image(183, 14, 'PlataformasAltas').setScale(1.5).setImmovable(true);
         this.platform6.body.setAllowGravity(false);
-        this.platform6.x=this.scale.width - 315;
-        this.platform6.y=265;
+        this.platform6.x = this.scale.width - 315;
+        this.platform6.y = 265;
 
-        this.platform7 = this.physics.add.image(this.scale.width/2, 14, 'PlataformasBajas').setScale(1.5).setImmovable(true);
+        this.platform7 = this.physics.add.image(this.scale.width / 2, 14, 'PlataformasBajas').setScale(1.5).setImmovable(true);
         this.platform7.body.setAllowGravity(false);
-        this.platform7.x= 150;
-        this.platform7.y=220;
+        this.platform7.x = 150;
+        this.platform7.y = 220;
 
-        this.platform8 = this.physics.add.image(this.scale.width/2 + 100, 14, 'PlataformasAltas').setScale(1.5).setImmovable(true);
+        this.platform8 = this.physics.add.image(this.scale.width / 2 + 100, 14, 'PlataformasAltas').setScale(1.5).setImmovable(true);
         this.platform8.body.setAllowGravity(false);
-        this.platform8.x= this.scale.width -150;
-        this.platform8.y=220;
+        this.platform8.x = this.scale.width - 150;
+        this.platform8.y = 220;
 
         //Colisiones 
         //Ania con limites de mundo
@@ -165,38 +165,77 @@ export class PantallaJuego extends Phaser.Scene {   //Crear clase que hereda de 
             D: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D),
             SPACE: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE),
             ENTER: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER),
-            Right:this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT),
-            Left:this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT)
+            Right: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT),
+            Left: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT)
         }
 
-    }
-    update(){
-            if(this.keys.D.isDown){ //Si presiona D
-                this.Ania.setVelocityX(160); //Se mueve a la derecha
-                this.Ania.anims.play('Anim_AniaWalk', true); //Reproducir animación de caminar
-                this.Ania.flipX=false; //No voltear sprite
-            }else if(this.keys.A.isDown){ //Si presiona A
-                this.Ania.setVelocityX(-160);//se mueve a la izquierda
-                this.Ania.anims.play('Anim_AniaWalk',true); //Reproducir animación de caminar
-                this.Ania.flipX=true;//Voltear sprite
+        //Cronometro
+        this.timerText = this.add.text(this.scale.width / 2, 30, "30", {
+            fontSize: "32px",       // Tamaño de la fuente
+            color: "#111111",       // Color del texto
+            fontWeight: "bold",     // Hacer la fuente más gruesa
+            stroke: "#000000",      // Color del contorno
+            strokeThickness: 2     // Grosor del contorno (más alto = más grueso)
+        });
 
-            }else{
-                this.Ania.setVelocityX(0); //No se mueve
-                this.Ania.anims.play('Anim_AniaIdle',true);//Reproducir animación de idle
-            }
-            if(this.keys.SPACE.isDown && this.Ania.body.touching.down){
-                this.Ania.setVelocityY(-350);//Salto
-            }
+        this.timerText.setOrigin(0.5, -0.2); // Centrar el texto horizontalmente
+        this.timerText.setDepth(10);         // Establecer la profundidad para asegurarse de que se dibuje encima de otros elementos
 
-            // Mover el gancho
-            if (this.keys.Right.isDown) {
-                this.Gancho.setVelocityX(160);
-            }
-            else if (this.keys.Left.isDown) {
-                this.Gancho.setVelocityX(-160);
-            }else{
-                this.Gancho.setVelocityX(0);
-            }
-        
+        // Configurar el temporizador
+        this.remainingTime = 30; // 30 segundos
+        this.time.addEvent({
+            delay: 1000, // Cada segundo
+            callback: this.updateTimer,
+            callbackScope: this,
+            loop: true,
+        });
+
     }
+    update() {
+        if (this.keys.D.isDown) { //Si presiona D
+            this.Ania.setVelocityX(160); //Se mueve a la derecha
+            this.Ania.anims.play('Anim_AniaWalk', true); //Reproducir animación de caminar
+            this.Ania.flipX = false; //No voltear sprite
+        } else if (this.keys.A.isDown) { //Si presiona A
+            this.Ania.setVelocityX(-160);//se mueve a la izquierda
+            this.Ania.anims.play('Anim_AniaWalk', true); //Reproducir animación de caminar
+            this.Ania.flipX = true;//Voltear sprite
+
+        } else {
+            this.Ania.setVelocityX(0); //No se mueve
+            this.Ania.anims.play('Anim_AniaIdle', true);//Reproducir animación de idle
+        }
+        if (this.keys.SPACE.isDown && this.Ania.body.touching.down) {
+            this.Ania.setVelocityY(-350);//Salto
+        }
+
+        // Mover el gancho
+        if (this.keys.Right.isDown) {
+            this.Gancho.setVelocityX(160);
+        }
+        else if (this.keys.Left.isDown) {
+            this.Gancho.setVelocityX(-160);
+        } else {
+            this.Gancho.setVelocityX(0);
+        }
+
+
+    }
+    updateTimer() {
+        this.remainingTime -= 1; // Decrementar el tiempo restante
+
+        // Actualizar el texto con el nuevo tiempo
+        this.timerText.setText(this.remainingTime);
+
+        // Verificar si el tiempo ha llegado a cero
+        if (this.remainingTime <= 0) {
+            this.timeUp(); // Llamar a la función para manejar el fin del tiempo
+        }
+    }
+
+    timeUp() {
+
+        this.scene.start("PantallaFinal"); // Cambiar a la escena ResultScreen
+    }
+
 }
